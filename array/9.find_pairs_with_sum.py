89a8d13f86907f154b16e8da6df3d4790f65a2c0
kir-an-n <207713602+kir-an-n@users.noreply.github.com>
2026-03-09 16:19:18 +0530

    Array series - problem 9 find pairs with sum

---
+++ array/9.find_pairs_with_sum.py
@@ -0,0 +1,22 @@
+# Problem 9 - Find Pairs with Sum
+# Given an array and target, find all pairs that add up to target
+# Example: [1, 2, 3, 4, 5] target = 6 → [(1, 5), (2, 4)]
+
+
+
+
+
+def find_pairs(arr, target):
+    pairs=[]
+
+    for i in range(len(arr)):
+        complement=target - arr[i]
+
+        if complement in arr and complement != arr[i]:
+
+            if (complement, arr[i]) not in pairs:
+                 pairs.append((arr[i], complement))
+
+    return pairs
+            
+print(find_pairs([1,2,3,4,5,6,7,8,9], 11))
