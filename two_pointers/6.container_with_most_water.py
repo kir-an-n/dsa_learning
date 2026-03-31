64730952230c8748f072995eac4fee7c39a039ee
kir-an-n <207713602+kir-an-n@users.noreply.github.com>
2026-03-31 16:54:58 +0530

    added container with most water

---
+++ two_pointers/6.container_with_most_water.py
@@ -0,0 +1,19 @@
+def container_with_most_water(arr):
+    left=0
+    right=len(arr)-1
+    max_area=0
+
+    while left < right:
+        
+        height=min(arr[left],arr[right])
+        width=right-left
+        current_area=height*width
+
+        max_area=max(max_area, current_area)
+
+        if arr[left]<arr[right]:
+            left+=1
+        else:
+            right-=1
+
+    return max_area
