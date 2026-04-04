536ddb241ae46d7b1956c24e8a8c8fdac030afea
kir-an-n <207713602+kir-an-n@users.noreply.github.com>
2026-04-04 15:06:29 +0530

    added new problem to binary search

---
+++ binary_search/5.sqrt_of_x.py
@@ -0,0 +1,15 @@
+def find_sqrt_ (x):
+    
+    low=1
+    high=x
+    ans=0
+    while low <=high :
+        mid=(low+high) //2
+        
+        if mid*mid= x
+           return x
+        elif mid*mid < x:
+             ans=mid
+             low=mid+1
+        else:
+            high=mid-1
